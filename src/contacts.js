32d8f4ef45f6015aca1ada1200c7514fd56407d6
OzgeAydin982 <80550373+OzgeAydin982@users.noreply.github.com>
2022-06-28 15:35:20 +0200

    Added initial files

---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Donald Knuth",
+    imgURL:
+      "https://www.premiosfronterasdelconocimiento.es/wp-content/uploads/sites/2/2019/06/FBBVA-10-tic-Knuth.jpg",
+    phone: "+123 456 789",
+    email: "d@donald.com"
+  },
+  {
+    name: "Robert Sedgewick",
+    imgURL: "https://sedgewick.io/wp-content/uploads/2021/09/ABOUT.jpg",
+    phone: "+987 654 321",
+    email: "r@robert.com"
+  },
+  {
+    name: "Dorothea Wagner",
+    imgURL:
+      "https://www.academia-net.org/sixcms/media.php/1252/thumbnails/DorotheaWagner.1097292.JPG.2775105.JPG",
+    phone: "+987 654 123",
+    email: "w@wagner.com"
+  }
+];
+
+export default contacts;
